22361cc1cd0a6bd81558810908db049c3ae468da
wuhao <wuhao939083405@126.com>
2018-12-15 21:36:52 +0800

    3

--- src/main/java/com/controller/QuestionController.java
+++ src/main/java/com/controller/QuestionController.java
@@ -7,6 +7,7 @@ import com.pojo.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class QuestionController {
     private QuestionRepository repository;
     //获取所有问题
     @RequestMapping(value = "/question")
+    @ResponseBody
     public List<Question> getAllQuestion(){
         return repository.findAll();
     }
